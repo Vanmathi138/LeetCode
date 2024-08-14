53fb96476e6077f70cfd545b58355f90c32e129d
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-08-14 12:30:10 +0530

    length of last word

--- src/LengthOfLastWord.java
+++ src/LengthOfLastWord.java
@@ -1,16 +1,24 @@
 public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
-        s = s.trim();
-        int lastSpaceIndex = s.lastIndexOf(' ');
+        int lastWordLength = 0;
+        boolean foundWord = false;
 
-        return s.length() - lastSpaceIndex - 1;
+        for(int i = s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                lastWordLength++;
+                foundWord = true;
+            }else if(foundWord){
+                break;
+            }
+        }
+        return lastWordLength;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(lengthOfLastWord("Hello World"));  // Output: 5
-        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));  // Output: 4
-        System.out.println(lengthOfLastWord("luffy is still joyboy"));  // Output: 6
-        System.out.println(lengthOfLastWord("a "));  // Output: 1
+        System.out.println("Output 1: "+lengthOfLastWord("Hello World"));  // Output: 5
+        System.out.println("Output 2: "+lengthOfLastWord("   fly me   to   the moon  "));  // Output: 4
+        System.out.println("Output 3: "+lengthOfLastWord("luffy is still joyboy"));  // Output: 6
+        System.out.println("Output 4: "+lengthOfLastWord("a "));  // Output: 1
     }
 }
