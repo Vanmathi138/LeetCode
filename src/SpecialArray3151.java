923fd498f22bbf77aba595e1b8ed3fac4f0f0661
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-08-28 17:37:59 +0530

    changes in special array

--- src/SpecialArray3151.java
+++ src/SpecialArray3151.java
@@ -1,6 +1,9 @@
 public class SpecialArray3151 {
     public static void main(String[] args) {
         int[] array = {1,2,3,4};
+        for(int i:array){
+            System.out.print(i+" ");
+        }
         System.out.println(array);
     }
 }
