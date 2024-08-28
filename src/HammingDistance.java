8352938503a485ebe96fac293adc6e9f61eaf97c
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-08-28 17:34:12 +0530

    special array

--- src/HammingDistance.java
+++
@@ -1,6 +0,0 @@
-public class HammingDistance {
-    public static void main(String[] args) {
-        int n = 79;
-        System.out.println(n);
-    }
-}
---
+++ src/SpecialArray.java
@@ -0,0 +1,2 @@
+public class SpecialArray {
+}
