0abd17229c38f752f0360c72d7fc46f4b513c39b
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-05 20:14:39 +0530

    reverse the words in a string

--- src/ReverseWordString.java
+++ src/ReverseWordString.java
@@ -1,2 +1,15 @@
+import java.util.*;
+
 public class ReverseWordString {
+    public static void main(String[] args) {
+        String s = "Hello World Java";
+        String result = reverse(s);
+        System.out.println(result);
+    }
+    public static String reverse(String s){
+        String[] word = s.trim().split("\\s+");
+        Collections.reverse(Arrays.asList(word));
+        return String.join(" ", word);
+
+    }
 }
