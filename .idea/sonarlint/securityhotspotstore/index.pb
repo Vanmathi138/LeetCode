261138a1eb676d64b082756578e486aae061966f
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-09 11:55:21 +0530

    find uncommon words from 2 string

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -67,3 +67,5 @@ H
 src/ClearDigits3174.java,6\8\68e8114d05bbe3caa9fcbfc6f115b4e2c35a7daf
 L
 src/NumberOfSegments434.java,e\c\ec3167738e01230253fa5254a4bd8bcf2cc80601
+F
+src/UnCommonWords.java,c\5\c577b4524702de7cdf76d6323173af6959cdc930
--- src/UnCommonWords.java
+++ src/UnCommonWords.java
@@ -1,2 +1,32 @@
+import java.util.*;
+import java.util.Map;
+
 public class UnCommonWords {
+    public static String[] uncommonFromSentences(String s1, String s2){
+        String[] word1 = s1.split(" ");
+        String[] word2 = s2.split(" ");
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for(String words : word1){
+            wordCount.put(words, wordCount.getOrDefault(words,0)+1);
+        }
+        for(String words : word2){
+            wordCount.put(words, wordCount.getOrDefault(words,0)+1);
+        }
+        List<String> unCommonWords = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry : wordCount.entrySet()){
+            if(entry.getValue() == 1){
+                unCommonWords.add(entry.getKey());
+            }
+        }
+        return unCommonWords.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "apple banana orange";
+        String s2 = "banana grape orange";
+
+        String[] result = uncommonFromSentences(s1, s2);
+        System.out.println(Arrays.toString(result));
+    }
 }
