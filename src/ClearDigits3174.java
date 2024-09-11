317a1af42a4c7543015d8b5031b7a8ee5802aee1
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-11 21:00:34 +0530

    clear digits

--- src/ClearDigits3174.java
+++ src/ClearDigits3174.java
@@ -1,5 +1,33 @@
+import java.util.Stack;
+
 public class ClearDigits3174 {
     public static void main(String[] args) {
+        String s = "abc7r8hhji8yj";
+        String result = clearDigits(s);
+        System.out.println(result);
+    }
+    public static String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
 
+        // Traverse the string character by character
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                // If we encounter a digit, pop the stack (remove the closest non-digit character)
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                // If it's a non-digit character, push it onto the stack
+                stack.push(c);
+            }
+        }
+
+        // Build the result string from the stack
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
