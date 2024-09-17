dafd6bba46e4887bbf696f27e788451d1e2e4a7f
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-17 20:52:05 +0530

    shuffle string

--- src/ShuffleString.java
+++ src/ShuffleString.java
@@ -1,8 +1,16 @@
 public class ShuffleString {
+    public static String restoreString(String s, int[] indices){
+        char[] shuffled = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+        return new String(shuffled);
+    }
+
     public static void main(String[] args) {
-        String a = "d";
-        String b = "d";
-
-
+        String s = "code";
+        int[] indices = {3, 1, 2, 0};
+        String result = restoreString(s, indices);
+        System.out.println(result);
     }
 }
