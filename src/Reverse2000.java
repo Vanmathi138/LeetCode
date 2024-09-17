8899ec121a16d9315795564b893794e06fe87f1f
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-17 20:07:42 +0530

    reverse

--- src/Reverse2000.java
+++ src/Reverse2000.java
@@ -1,17 +1,13 @@
 public class Reverse2000 {
     public static String reversePrefix(String word, char ch) {
-        // Find the index of the first occurrence of 'ch'
         int index = word.indexOf(ch);
 
-        // If 'ch' is found, reverse the prefix up to 'index'
         if (index != -1) {
-            // Reverse the prefix part and concatenate with the rest of the string
             StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
             sb.reverse();
             return sb.toString() + word.substring(index + 1);
         }
 
-        // If 'ch' is not found, return the original word
         return word;
     }
 
