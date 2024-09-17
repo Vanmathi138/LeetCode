97b544825fda7d7ffeb9fdd7a966a8bf2940c337
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-09-17 20:34:42 +0530

    shuffle the string

--- src/NumberOfPairs2023.java
+++ src/NumberOfPairs2023.java
@@ -1,3 +1,17 @@
 public class NumberOfPairs2023 {
-    
+    public static String restoreString(String s, int[] indices) {
+        char[] shuffled = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+
+        return new String(shuffled);
+    }
+
+    public static void main(String[] args) {
+        int[] indices = {3, 1, 2, 0};
+        String s = "code";
+        String result = restoreString(s,indices);
+        System.out.println(result);  // Output: "edoc"
+    }
 }
