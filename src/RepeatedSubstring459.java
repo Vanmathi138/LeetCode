150ec8b4f8d60b1d3430f46e83773f9a397b2ff8
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-10-08 11:03:36 +0530

    repeated substring

--- src/RepeatedSubstring459.java
+++ src/RepeatedSubstring459.java
@@ -1,2 +1,16 @@
 public class RepeatedSubstring459 {
+    public static void main(String[] args) {
+        String s = "aba";
+        String f = "abab";
+
+        System.out.println(repeatedSubstringPattern(s));
+        System.out.println(repeatedSubstringPattern(f));
+    }
+
+    private static boolean repeatedSubstringPattern(String s) {
+
+        String d = s+s;
+
+        return d.substring(1, d.length()-1).contains(s);
+    }
 }
