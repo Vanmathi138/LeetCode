d5cf9204ba390033a1a9ef99fcefde188c212e6c
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-10-10 10:03:05 +0530

    summary ranges

--- src/SummaryRanges.java
+++ src/SummaryRanges.java
@@ -1,2 +1,39 @@
+import java.util.*;
+
 public class SummaryRanges {
+
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int start = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+
+                start = nums[i];
+            }
+        }
+
+        if (start == nums[nums.length - 1]) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + nums[nums.length - 1]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 4, 5, 7};
+        System.out.println(summaryRanges(nums));  // Output: [0->2, 4->5, 7]
+    }
 }
