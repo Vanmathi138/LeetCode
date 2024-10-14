df36251bba086958f8c481b841d99baf659d0437
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-10-14 22:01:10 +0530

    lemonade change

--- src/LemonadeChange.java
+++ src/LemonadeChange.java
@@ -1,2 +1,35 @@
 public class LemonadeChange {
+    public static boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills1 = {5, 5, 5, 10, 20};
+        int[] bills2 = {5,5,10,10,20};
+        System.out.println(lemonadeChange(bills1));
+        System.out.println(lemonadeChange(bills2));
+    }
 }
