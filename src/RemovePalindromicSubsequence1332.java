124c35aab303c051dba4a25ee08aad4250a6aab7
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-10-23 11:09:35 +0530

    remove palindrome subsequence

--- src/RemovePalindromicSubsequence1332.java
+++ src/RemovePalindromicSubsequence1332.java
@@ -1,2 +1,31 @@
 public class RemovePalindromicSubsequence1332 {
+        public static int removePalindromeSub(String s) {
+
+            if (isPalindrome(s)) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+
+        private static boolean isPalindrome(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+    public static void main(String[] args) {
+        String s1 = "ababa";
+        int result = removePalindromeSub(s1);
+        String s2 = "abab";
+        System.out.println(removePalindromeSub(s2));
+        System.out.println(result);
+    }
 }
