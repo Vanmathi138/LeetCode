b091abbbf8ea6d03a8cec99048116ba521f81bd5
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-11-11 11:25:10 +0530

    move zeros

--- src/MoveZeros283.java
+++ src/MoveZeros283.java
@@ -1,17 +1,15 @@
 public class MoveZeros283 {
     public static void moveZeroes(int[] nums) {
-        int lastNonZeroFoundAt = 0;
+        int nonZeroFoundAt = 0;
 
-        // Move all the non-zero elements to the beginning of the array.
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[lastNonZeroFoundAt] = nums[i];
-                lastNonZeroFoundAt++;
+                nums[nonZeroFoundAt] = nums[i];
+                nonZeroFoundAt++;
             }
         }
 
-        // Fill the rest of the array with 0s.
-        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+        for (int i = nonZeroFoundAt; i < nums.length; i++) {
             nums[i] = 0;
         }
     }
@@ -19,8 +17,9 @@ public class MoveZeros283 {
     public static void main(String[] args) {
         int[] num = {1,0,5,0,6};
         moveZeroes(num);
+        System.out.println("After moving zeros to the end of array:");
         for (int i : num){
-            System.out.print(i+" ");
+            System.out.print(i+",");
         }
     }
 
