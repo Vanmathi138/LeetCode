72d3ff872cd9ecd96303a8a759a271e1493f4aee
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-11-14 11:14:42 +0530

    method reference

--- src/Sample.java
+++ src/Sample.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 @FunctionalInterface
 interface A{
     // ABSTRACT METHOD
@@ -12,17 +14,54 @@ interface A{
 }
 
 public class Sample {
-    public static void main(String[] args) {
+    public static void printNames(String a){
+        System.out.println("name : "+a );
+    }
 
+    public static void main(String[] args) {
+        //old method
+        System.out.println();
         A obj1 = new A(){
             public void display() {
                 System.out.println("This is a Functional Interface");
             }
         };
         obj1.display();
-
+        System.out.println();
+        // USING LAMBDA EXPRESSION
         A obj2 = () -> System.out.println("lamda expression");
         obj2.display();
+        System.out.println();
+
+        // METHOD REFERENCE EXAMPLE
+        List<String> arr = Arrays.asList("Apple","Orange","Grape","Tomato");
+
+        // normal for each
+        for(String name : arr){
+            printNames(name);
+
+        }
+
+        //using lambda
+        arr.forEach(name -> printNames(name));
+
+        //using method reference
+        arr.forEach(System.out::println);
+        arr.forEach(Sample::printNames);
 
     }
 }
+//for (String names : arr){
+//printNames(names);
+//        }
+//                System.out.println();
+//
+//        arr.forEach(name -> System.out.println(name)); //lambda exp
+//        arr.forEach(name -> printNames(name));
+//
+//        System.out.println();
+//
+//        System.out.println("using method reference");
+//        arr.forEach(System.out::println);
+//        System.out.println();
+//        arr.forEach(Sample::printNames);
