64d379dcec43b1913dd4f4e371394c89e0443127
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-11-14 17:28:29 +0530

    find the difference

--- src/FindTheDiff389.java
+++ src/FindTheDiff389.java
@@ -1,2 +1,26 @@
 public class FindTheDiff389 {
+    public static void main(String[] args) {
+        String a = "abcd";
+        String b = "abcde";
+        char c = findTheDifference(a,b);
+        System.out.println(c);
+    }
+
+    private static char findTheDifference(String a, String b) {
+        int sumA = 0;
+        int sumB = 0;
+
+        for(char i : a.toCharArray()){
+            sumA += i;
+        }
+        for(char j : b.toCharArray()){
+            sumB += j;
+        }
+        if(sumA > sumB){
+            return (char)(sumA-sumB);
+        }else{
+            return (char) (sumB- sumA);
+        }
+
+    }
 }
