af8738c36d8574d21f3f52cddd3e08d3d807c9f2
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-11-23 11:33:33 +0530

    array partition

--- src/ArrayPartition561.java
+++ src/ArrayPartition561.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class ArrayPartition561 {
     public static int arrayPairSum(int[] nums) {
-        // Step 1: Sort the array
+
         Arrays.sort(nums);
 
         int sum = 0;
-        // Step 2: Sum up the elements at even indices
+
         for (int i = 0; i < nums.length; i += 2) {
             sum += nums[i];
         }
@@ -17,6 +17,9 @@ public class ArrayPartition561 {
     public static void main(String[] args) {
         int[] nums = {1, 4, 3, 2};
         int result = arrayPairSum(nums);
-        System.out.println(result);  // Output: 4
+        System.out.println(result);
+
+        int[] nums2 = {6, 2, 6, 5, 1, 2};
+        System.out.println("Output: " + arrayPairSum(nums2));
     }
 }
--- src/CountElements3005.java
+++ src/CountElements3005.java
@@ -24,13 +24,13 @@ public class CountElements3005 {
 
     public static void main(String[] args) {
 
-        int[] nums1 = {1, 3, 3, 2, 2, 2};
+        int[] nums1 = {1, 4, 4, 2, 2};
         System.out.println(countElementsWithMaxFrequency(nums1)); 
 
-        int[] nums2 = {5, 5, 5, 2, 2};
+        int[] nums2 = {1, 2, 3, 4, 5};
         System.out.println(countElementsWithMaxFrequency(nums2));
 
-        int[] nums3 = {7, 8, 8, 9, 9, 9};
+        int[] nums3 = {1, 1, 2, 4, 4, 4};
         System.out.println(countElementsWithMaxFrequency(nums3));
     }
 }
